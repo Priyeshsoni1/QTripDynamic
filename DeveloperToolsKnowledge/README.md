b541df70ae7cea61adf39bab97491d06c73211df
Priyesh Soni <priyesh9792@gmail.com>
2023-09-16 21:20:05 +0530

    developersToolKit

---
+++ DeveloperToolsKnowledge/README.md
@@ -0,0 +1,1 @@
+The Chrome browser provides tools to help developers easily perform tasks like inspecting web page content, analysing HTTP requests made and checking data stored locally by web pages.
---
+++ DeveloperToolsKnowledge/me_developer_tools_ii/index.js
@@ -0,0 +1,75 @@
+// const getApiData=()=>{
+//     const url="https://run.mocky.io/v3/511fa794-6bfb-4c9f-9adb-9e18d62d7003"
+//     fetch(url).then((Response)=>Response.json()).then((data)=>console.log(data))
+// }
+// getApiData()
+
+
+const getApiData=async()=>{
+    try{
+    let res=await fetch("https://run.mocky.io/v3/511fa794-6bfb-4c9f-9adb-9e18d62d7003")
+    let data=await res.json();
+   
+    return data;
+}
+    catch(error)
+    {
+        console.log(error);
+        throw error
+    }
+
+}
+
+
+const getTableRow=(title,acRate,difficulty)=>{
+    let tRow=document.createElement("tr");
+    tRow.innerHTML=
+    `<td>${title}</td>
+    <td>${acRate}</td>
+    <td>${difficulty}</td>
+    `
+    return tRow;
+}
+
+const populateTable=(questionsList)=>{
+
+
+ console.log(questionsList)
+
+ const body = document.getElementById("table-body");
+
+     questionsList.forEach((data)=>{
+     console.log(data)
+        const {title,acRate,difficulty}=data;
+   
+       const rowData= getTableRow(title,acRate,difficulty);
+      
+       body.appendChild(rowData)
+     })
+
+
+    
+
+
+}
+
+let questionsList = await getApiData()
+ questionsList=questionsList.problemsetQuestionList.questions
+populateTable(questionsList)
+
+const sortQuestionsByAcceptanceRate=(questionsList)=>{
+    let sortedArray=questionsList.sort((a,b)=>b.acRate-a.acRate)
+    return sortedArray
+
+}
+
+
+
+
+let acceptanceHeaderElement=document.querySelector("#questions-table > thead > tr > th:nth-child(2)")
+acceptanceHeaderElement.addEventListener('click',()=>{
+    const sortedArray=sortQuestionsByAcceptanceRate(questionsList)
+
+    populateTable(sortedArray);
+    
+})
